f0e090062176e1e016bcf78cb5e0b57ec7844ab5
mj.joo <noonsollin@gmail.com>
2026-06-17 01:15:04 +0900

    python sample Example

---
+++ LogViewer/src_py/for_range.py
@@ -0,0 +1,13 @@
+# for 반복문과 범위를 함께 조합해서 사용합니다.
+for i in range(5):   # 0부터 5-1 까지의 정수로 범위 만듦
+    print(str(i) + "= 반복 변수")
+print()
+
+
+for i in range(5,10):  # 5부터 10-1 까지의 정수로 범위 만듦
+    print(str(i) + "= 반복 변수")
+print()
+
+for i in range(0,10,3): # 0부터 10-1 까지의 정수를 범위로 만드는데, 앞 뒤의 숫자가 3만큼 차이를 가짐
+    print(str(i) + "= 반복 변수")
+print()
