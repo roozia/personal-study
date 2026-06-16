3b0ddc16e61f0d39ce0939afea447fbddd9604e4
mj.joo <noonsollin@gmial.com>
2026-06-16 17:23:00 +0900

    Sample Log 파일 추가 및 properties 상대경로로 변경

--- LogViewer/src_py/step_01_loadConfigFile.py
+++ LogViewer/src_py/step_01_loadConfigFile.py
@@ -3,11 +3,11 @@
 import configparser
 
 
-print()
-print()
+print() #공백문자
+print() #공백문자
 
 # Config 파일 읽기
-properties_file = "C:/PythonStudy/properties/logviewer.properties"
+properties_file = "./logviewer.properties"
 file_path=""
 context_line=0
 keyword=""
@@ -19,6 +19,9 @@ parser.read_string(p_lines)
 
 file_path = parser.get("DEFAULT", "logviewer.file.path",  fallback='').strip()
 context_line = parser.get("DEFAULT", "logviewer.context.lines",  fallback='').strip()
+with open(file_path, "r", encoding="utf-8") as log:
+    l_lines = log.readlines()
+
 keyword = parser.get("DEFAULT", "logviewer.condition.keywords",  fallback='').strip()
 
 print("filePath = " ,file_path)
@@ -28,8 +31,6 @@ print("keyword = ", keyword)
 print("1. ----------------------------")
 
 extract_list = ""
-with open(file_path, "r", encoding="utf-8") as log:
-    l_lines = log.readlines()
 
 
     for line in l_lines :
